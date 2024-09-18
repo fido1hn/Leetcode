b36ad7a5f6f644beac34345a4c59b320a7d5f7a0
Confidence A.E. <confidoekeanya@gmail.com>
2024-09-18 06:14:06 +0100

    1372. Longest ZigZag Path in a Binary Tree

---
+++ 1372-Longest-ZigZag-Path-in-a-Binary-Tree/1372-Longest-ZigZag-Path-in-a-Binary-Tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var longestZigZag = function (root) {
+  const helper = (root, isLeft, depth) => {
+    if (!root) return depth;
+
+    if (isLeft) {
+      depth = Math.max(
+        depth,
+        helper(root.right, false, depth + 1),
+        helper(root.left, true, 0)
+      );
+    } else {
+      // right
+      depth = Math.max(
+        depth,
+        helper(root.left, true, depth + 1),
+        helper(root.right, false, 0)
+      );
+    }
+
+    return depth;
+  };
+
+  return Math.max(helper(root.left, true, 0), helper(root.right, false, 0));
+};
